f9f27a0026d31896858fa676d4aa7a2ca76d5ad8
NithyaKumarasamy <30834907+NithyaKumarasamy@users.noreply.github.com>
2017-08-08 07:06:36 -0700

    first.c

---
+++ first.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a=122;
+if(a<0)
+{
+printf("the num is negative");
+}
+elseif(a>0)
+{
+printf("the num is passitive");
+}
+else
+{
+printf("the given num is zero");
+}
+}
