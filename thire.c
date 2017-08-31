687b18102f58718340b31d92e861278e178d6746
NithyaKumarasamy <30834907+NithyaKumarasamy@users.noreply.github.com>
2017-08-31 07:30:04 -0700

    Create thire.c

---
+++ thire.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+int n;
+printf("enter the character");
+scanf("%c",&n);
+if(n={'a','e','i','o','u'})
+printf("the given character is vowel");
+}
+else
+{
+prinf("the given character is consonant');
+}
+}
