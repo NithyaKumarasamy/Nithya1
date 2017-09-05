36df397fe76301bb328125b2ac2f149ad52f832f
NithyaKumarasamy <30834907+NithyaKumarasamy@users.noreply.github.com>
2017-09-05 16:55:08 +0530

    Create largest.c

---
+++ largest.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+int a,b,c;
+printf("enter the 3 numbers");
+scanf("%d %d %d",&a,&b,&c);
+if(a>b&&a>c)
+{
+printf("a is largest number");
+}
+elseif(b>c)
+{
+printf("b is gratest number");
+}
+else
+{
+printf("c is largest number"0;
+}
+}
