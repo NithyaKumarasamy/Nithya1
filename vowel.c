ed009d043773ff1d62409f60510fd01f92f4f3b7
NithyaKumarasamy <30834907+NithyaKumarasamy@users.noreply.github.com>
2017-09-05 16:20:54 +0530

    Create vowel.c

---
+++ vowel.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+char a;
+printf("enter the character");
+scanf("%c",c);
+if(c=(a>='a' && a<=z)&&(a>=A && a<=z)
+{
+printf("character is alphabet");
+}
+else
+{
+printf("the character is consonaunt");
+}
+}
