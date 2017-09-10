ecd97333bb12494d0d02eccd8b1da27439952be3
NithyaKumarasamy <30834907+NithyaKumarasamy@users.noreply.github.com>
2017-09-10 02:48:00 -0700

    Update number.c

--- number.c
+++ number.c
@@ -1,12 +1,15 @@
-#include<stdio.h>
+#include<stdio.h> 
 void main()
 {
-int n,i,sum=0;
-printf("enter the passitive integer");
-scanf("%d",&n);
-for(i=1;i<=n;++i)
-{
-sum=sum+i;
-}
-printf("sum=%d",sum);
-}
+  int n,sum=0,i;
+  printf(:enter the value of n");
+  scanf("%d",&n);
+  for(i=0;i<=n;++i)
+         {
+           sum=sum+i;
+         }
+         printf("sum=%d",sum);
+         }
+         }
+          
+  
