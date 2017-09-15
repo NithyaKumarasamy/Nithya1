25b1183ccb4b9f155113a35d6c992f19547c8ecd
NithyaKumarasamy <30834907+NithyaKumarasamy@users.noreply.github.com>
2017-09-15 06:48:59 -0700

    Create factorial.c

---
+++ factorial.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+int i,n;
+unsignd long long factorial=1;
+ptintf("enter the passitive integer");
+scanf("%d",&n);
+if(n<0)
+{
+printf("error we not find factorial for negative number");
+}
+else
+{
+for(i=1;i<=n;i++)
+{
+factorial=*i;
+}
+printf("the factorial number",factorial);
+}
+return 0;
+}
