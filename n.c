e2865d6dea238674b8d71c02bf2f4b40a521cb30
NithyaKumarasamy <30834907+NithyaKumarasamy@users.noreply.github.com>
2018-01-11 16:31:22 +0530

    Update n.c

--- n.c
+++ n.c
@@ -5,4 +5,17 @@ int array[50]
 printf("enter the size of the array");
 scanf("%d",&size);
 printf("enter the number of elements");
+  for(i=0;i<size;i++)
+  {
+ scanf("%d",&array[i]);
+  }
+  largest=array[0];
+  for(i=1;i<size;i++)
+  {
+    if(largest<
+  {
+  largest=array[i];
+  }
+  printf("the largest number is %d",largest);
+}
 
