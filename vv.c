6c11cc4c74a91a7b06a84f91b30aa7f927fca183
NithyaKumarasamy <30834907+NithyaKumarasamy@users.noreply.github.com>
2018-01-18 16:28:53 +0530

    Create vv.c

---
+++ vv.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int low,high
+printf("enter the low and high value");
+scanf("%d%d",&low,&high)
+while(low<high)
+{
+flag=0;
+for(i=2;i<=low/2;++i)
+{
+if(low%2==0)
+{
+flag=1;
+if(flag==0
