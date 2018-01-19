6e73f1c7da88eec559a8b51f8b759e7e0d2e33a8
NithyaKumarasamy <30834907+NithyaKumarasamy@users.noreply.github.com>
2018-01-19 16:03:23 +0530

    sort.c

---
+++ sort.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+int n,i,a[i];
+printf("enter the number of elemen you want");
+scanf("%d",&n);
+printf("the numbers are");
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+printf("%d\n",a[i]);
+}
+for(i=0;i<n;i++)
+{
+for(j=i+1;i<n;j++)
+{
