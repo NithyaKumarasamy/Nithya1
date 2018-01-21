66e968fb28a461cc860b55eef09b980ec92ba67d
NithyaKumarasamy <30834907+NithyaKumarasamy@users.noreply.github.com>
2018-01-21 12:24:03 +0530

    Create num.c

---
+++ num.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+
+printf("enter the number of elements u want");
+scanf("%d",&n);
+printf("th enumbers are");
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<n;i++0
+{
+for(j=i+1;j<n;j++)
+{
