552bf438f303df5a682b87cbfee303b0f3a24285
NithyaKumarasamy <30834907+NithyaKumarasamy@users.noreply.github.com>
2018-01-22 06:13:01 -0800

    nu.c

---
+++ nu.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+
+printf("enter the number of sring u want");
+scanf("%d",&n);
+printf("enter the %d elemens",n);
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<n;i++)
+{
+
+
+
+
