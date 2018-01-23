8c804b535e793057b4da54cb6bda320265f4f16c
NithyaKumarasamy <30834907+NithyaKumarasamy@users.noreply.github.com>
2018-01-23 15:43:28 +0530

    Update nu.c

--- nu.c
+++ nu.c
@@ -15,6 +15,12 @@ if(s[i]=='0'||s[i]=='1'||s[i]=='2'||s[i]=='3'||s[i]=='4'||s[i]=='5'||s[i]=='6'||
   if(flag==0)
   {
     printf("the given number not number");
+  }
+  else
+  {
+    printf("string is not a number");
+  }
+}
   
   
 
