805e9be57269e1218a3da21cbf1a499e9abe2037
NithyaKumarasamy <30834907+NithyaKumarasamy@users.noreply.github.com>
2018-01-23 16:10:21 +0530

    count.c

---
+++ count.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+char s[10],i,count=0;
+printf("enter the string");
+scanf("%s",s);
+for(i=0;s[i]='\0';i++)
+{
+if(s[i]==' ')
+count++;
+}
+printf("the total words are%d",count+1);
+}
