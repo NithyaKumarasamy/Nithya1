6818842c36bdc46310634a7950717e58be2cca81
NithyaKumarasamy <30834907+NithyaKumarasamy@users.noreply.github.com>
2018-02-09 09:29:19 +0000

    swap2.c

---
+++ swap2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+  int a,b,temp=0;
+  printf("enter the 2 nubmber");
+  scanf("%d %d",&a,&b);
+  temp=a;
+  a=b;
+  b=temp;
+  printf("after swapping%d %d",a,b);
+  return 0;
+}
+
+  
+  
