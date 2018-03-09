85b2f13987fb28232d90feb8ecf17f53d8134ad0
NithyaKumarasamy <30834907+NithyaKumarasamy@users.noreply.github.com>
2018-03-09 16:51:25 +0530

    Create intrest.c

---
+++ intrest.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int a,t;
+    float r,si;
+    printf("enter amount,time,rate");
+    scanf("%d %d %f",&a,&t,&r);
+    si=(a*t*r)/100;
+    printf("%f",si);
+}
+
+
+
