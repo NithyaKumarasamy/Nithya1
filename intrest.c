2225423239eb877275248d8b9aad924777658f3d
NithyaKumarasamy <30834907+NithyaKumarasamy@users.noreply.github.com>
2018-03-13 16:24:49 +0530

    Update intrest.c

--- intrest.c
+++ intrest.c
@@ -1,15 +1,12 @@
-
-#include <stdio.h>
-
+#include<stdio.h>
+#include<math.h>
 int main()
 {
     int a,t;
-    float r,si;
+    double si;
+    float r;
     printf("enter amount,time,rate");
     scanf("%d %d %f",&a,&t,&r);
     si=(a*t*r)/100;
-    printf("%f",si);
+    printf("%f",floor(si));
 }
-
-
-
